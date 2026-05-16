1b6e54c6e528acd62896bd622fbe1d87abfe66e3
TalysonRodrigues-Dev <157434539+TalysonRodrigues-Dev@users.noreply.github.com>
2026-05-16 00:37:45 +0000

    adicionando script `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Repositório para replicar o tabnews.com.br, afim de estudos de projetos do curso.dev
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 function Home() {
-    return <h1>Oque eu quero, eu consigo, tenho fé!</h1>
+  return <h1>Oque eu quero, eu consigo, tenho fé!</h1>;
 }
-
+function teste() {
+  console.log("teste");
+}
 export default Home;
