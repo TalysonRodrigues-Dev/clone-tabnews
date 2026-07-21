12d67f86906a5219611848acbd2008686ba34afe
TalysonRodrigues-Dev <157434539+TalysonRodrigues-Dev@users.noreply.github.com>
2026-07-21 02:27:50 +0000

    finish status endpoint

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,35 @@
 import database from "infra/database.js";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1;");
-  console.log(result);
-  response.status(200).json({ chave: "Deu tudo certo" });
+  const updatedAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersionValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxconnectionValue =
+    databaseMaxConnectionResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTFRES_DB;
+  const databaseOpenedConnectionsResut = await database.query({
+    text: "SELECT count(*)::int FROM pg_stat_activity WHERE datname = $1 ;",
+    values: [databaseName],
+  });
+  const databaseOpenedConnectionsValue =
+    databaseOpenedConnectionsResut.rows[0].count;
+
+  response.status(200).json({
+    updated_at: updatedAt,
+    dependencies: {
+      database: {
+        version: databaseVersionValue,
+        max_connections: parseInt(databaseMaxconnectionValue),
+        pened_connections: databaseMaxconnectionValue,
+      },
+    },
+  });
 }
 
 export default status;
